a998d912377658bf321fb35b28e31453b393a6d6
Rahul <deyr279@gmail.com>
2024-02-22 00:57:44 +0530

    added react sample components

---
+++ ui/src/example-component/card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card } from 'antd';
+
+
+const ExampleCard = () => {
+    /**
+ * The following shows a HOC (Higher Order Component)
+ * that wraps the Calender component
+ */
+    const style = {
+        width: '300'
+    }
+    return (
+        <div style={style}>
+            <Card title="Example Card" bordered={false}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+            </Card>
+        </div>
+    )
+}
+
+export default ExampleCard;
